8f849f4936e9b6916a8eb6a9f821baff104b2be2
Zhang Hainan <zhanghn@han-sky.com>
2017-07-06 09:01:12 +0800

    update 0706

---
+++ Reader/Coding/Component/ZHNBookLodingView/ZHNBookLoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  ZHNBookLoadingView.swift
+//  Reader
+//
+//  Created by 张海南 on 2017/7/5.
+//  Copyright © 2017年 枫韵海. All rights reserved.
+//
+
+import UIKit
+
+class ZHNBookLoadingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
