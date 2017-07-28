ec9b67457df2d0f7be63173651e7410b1f101381
Zhang Hainan <zhanghn@han-sky.com>
2017-07-28 17:48:14 +0800

    update version 1.2.1

--- Reader/Coding/Common/Constant.swift
+++ Reader/Coding/Common/Constant.swift
@@ -12,7 +12,7 @@ import Foundation
 //let HOST = "http://192.168.1.104:9090"
 //let HOST = "http://localhost:9091"
 let HOST = "http://192.168.6.177:8090"
-//let HOST = "http://47.94.86.211:8080/mytest/"
+//let HOST = "http://47.94.86.211:8080/new_novel/"
 
 let kSeparatorViewH: CGFloat = 0.5
 
